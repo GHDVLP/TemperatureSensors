6d276b57de38102483bb7ff39f57cf987b9e7b60
GHDVLP <f_b2002@bk.ru>
2023-05-20 14:13:15 +0500

    Обнова
    Получение значений со scale, добавление label, импорт matplotlib

--- Scada.py
+++ Scada.py
@@ -1,8 +1,14 @@
 from tkinter import*
+from matplotlib import*
 from PIL import*
 class Cnopka(Button):
     def __init__(self, master, text, width, height, bg = 'grey', **kw):
         super().__init__(master = master, text = text, width = width, height = height, bg = bg, **kw)
+
+# функция съема значения с ползунка
+def scaleget(newVal):
+    label['text'] = float(newVal)
+
 # главное окно
 main = Tk()
 main.state('zoomed')
@@ -22,7 +28,9 @@ btn1 = Cnopka(frame, 'Вкл. вентилирование', 21, 2)
 btn.place(x=1011, y=619)
 btn1.place(x=1010, y=662)
 # ползунок
-scale = Scale(frame, bg='grey', activebackground='white', highlightbackground='MistyRose1', from_= 100, to=0, width=50, length=220)
+scale = Scale(frame, orient='vertical', from_= 100, to=0, width=53, length=240, showvalue=0, sliderlength=10, sliderrelief='raised', tickinterval = 25, command=scaleget)
 scale.place(x=1057, y=368)
+label = Label(frame, width=11, height=1)
+label.place(x=1057, y=348)
 
 main.mainloop()
