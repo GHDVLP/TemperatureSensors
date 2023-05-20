f15dd84f31227f32023c5066814c2fcc69ba4d86
GHDVLP <f_b2002@bk.ru>
2023-05-20 15:02:16 +0500

    Update
    Убрали класс кнопок, создали проверку текущего состояния кнопок

--- Scada.py
+++ Scada.py
@@ -1,13 +1,25 @@
 from tkinter import*
+import tkinter
 from matplotlib import*
 from PIL import*
-class Cnopka(Button):
-    def __init__(self, master, text, width, height, bg = 'grey', **kw):
-        super().__init__(master = master, text = text, width = width, height = height, bg = bg, **kw)
-
+CountBtn = 1
 # функция съема значения с ползунка
 def scaleget(newVal):
-    label['text'] = float(newVal)
+    currentValue['text'] = newVal
+def BtnChangeState(text):
+    global CountBtn
+    if text == '1':
+        CountBtn += 1
+        if CountBtn % 2 == 0:
+            btn1['state'] = 'disabled'
+        else:
+            btn1['state'] = 'normal'
+    if text == '2':
+        CountBtn += 1
+        if CountBtn % 2 == 0:
+            btn['state'] = 'disabled'
+        else:
+            btn['state'] = 'normal'
 
 # главное окно
 main = Tk()
@@ -23,14 +35,15 @@ label = Label(frame, image=photo)
 label.place(x=0, y=0)
 frame.pack()
 # кнопки
-btn = Cnopka(frame, 'Авт. вентилирование', 21, 2)
-btn1 = Cnopka(frame, 'Вкл. вентилирование', 21, 2)
+btn = Button(frame, text='Авт. вентилирование', width=21, height=2, state = None, command=lambda: BtnChangeState('1'))
+btn1 = Button(frame, text='Вкл. вентилирование', width=21, height=2, state = None, command=lambda: BtnChangeState('2'))
 btn.place(x=1011, y=619)
 btn1.place(x=1010, y=662)
 # ползунок
-scale = Scale(frame, orient='vertical', from_= 100, to=0, width=53, length=240, showvalue=0, sliderlength=10, sliderrelief='raised', tickinterval = 25, command=scaleget)
+scale_tittle = Label(frame, text = 'Нагрев %', width=10, height=1, bg = 'grey80')
+scale_tittle.place(x = 1057, y = 348)
+scale = Scale(frame, orient='vertical', from_= 100, to=0, width=70, length=220, showvalue=0, sliderlength=20, sliderrelief='raised', command=scaleget)
 scale.place(x=1057, y=368)
-label = Label(frame, width=11, height=1)
-label.place(x=1057, y=348)
-
+currentValue = Label(frame, width=6, height=14, bg = 'grey80', highlightbackground = 'black')
+currentValue.place(x=1012, y=371)
 main.mainloop()
