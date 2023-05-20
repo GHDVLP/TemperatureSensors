089102c8aed5fc663ba9d69700f40e1a0c5ceec1
GHDVLP <f_b2002@bk.ru>
2023-05-20 13:32:04 +0500

    Changes

--- Scada.py
+++ Scada.py
@@ -1,3 +1,16 @@
 from tkinter import*
 from PIL import*
 
+main = Tk()
+main.state('zoomed')
+main.title("SCADA система температурных датчиков")
+window_width = 800
+window_height = 600
+main.geometry('%dx%d' % (window_width, window_height))
+
+photo = PhotoImage(file='1.png')
+frame = Frame(main, width=1920, height=1080, bg='MistyRose1')
+label = Label(frame, image=photo)
+label.place(x=0, y=0)
+frame.pack()
+main.mainloop()
