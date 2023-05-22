e7ff1a93c4045e2803d1545ddaf3c5929dde6df6
GHDVLP <f_b2002@bk.ru>
2023-05-23 01:38:29 +0500

    обновление 1.03
    логика для отрицательных значений

--- Scada.py
+++ Scada.py
@@ -16,7 +16,7 @@ SensorTemp_TPL_array = [20.0]
 SensorTemp_TPK_array = [20.0]
 SensorTemp_RT100_array = [20.0]
 
-start_time = datetime.now()
+start_time = datetime.now(tz=None)
 CountBtnClck = 1
 CountBtnClck1 = 1
 CountBtnClck2 = 1
@@ -51,6 +51,7 @@ def BtnChangeState(text):
         if CountBtnClck2 % 2 == 0:
             btn['bg'] = 'green'
             create_plot()
+            update_plot()
         else:
             btn['bg'] = 'grey'
             destroy_plot()
@@ -62,60 +63,69 @@ def BtnChangeState(text):
         else:
             RedrawBtn['bg'] = 'grey'
 
-# обновление графика
+# создание графика
 def create_plot():
     global fig, plot, canvas
     fig = Figure(figsize=(7,4), dpi=100)
     plot = fig.add_subplot(1, 1, 1)
     canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.get_tk_widget().place(x=150, y=150)
-    def update_plot():
-        global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array
-        elapsed_time = datetime.now() - start_time
-        x = elapsed_time.total_seconds()
-        if CountBtnClck % 2 == 1:
-            SensorTemp_RTD_array.append(scale.get()*0.01+SensorTemp_RTD_array[-1])
-            SensorTemp_Cuprum_array.append(scale.get()*0.02+SensorTemp_Cuprum_array[-1])
-            SensorTemp_TPL_array.append(scale.get()*0.03+SensorTemp_TPL_array[-1])
-            SensorTemp_TPK_array.append(scale.get()*0.04+SensorTemp_TPK_array[-1])
-            SensorTemp_RT100_array.append(scale.get()*0.05+SensorTemp_RT100_array[-1])
-        if CountBtnClck % 2 == 0:
-            if SensorTemp_RTD_array[-1] <= 0:
-                SensorTemp_RTD_array[-1] = 0
-            if SensorTemp_Cuprum_array[-1]  <= 0:
-                SensorTemp_Cuprum_array[-1]  = 0
-            if SensorTemp_TPL_array[-1] <= 0:
-                SensorTemp_TPL_array[-1] = 0
-            if SensorTemp_TPK_array[-1] <= 0:
-                SensorTemp_TPK_array[-1] = 0
-            if SensorTemp_RT100_array[-1] <= 0:
-                SensorTemp_RT100_array[-1] = 0
-
+# обновление графика
+def update_plot():
+    global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array, CountBtnClck2
+    elapsed_time = datetime.now() - start_time
+    x = elapsed_time.total_seconds()
+    if CountBtnClck % 2 == 1:
+        SensorTemp_RTD_array.append(scale.get()*0.01+SensorTemp_RTD_array[-1])
+        SensorTemp_Cuprum_array.append(scale.get()*0.02+SensorTemp_Cuprum_array[-1])
+        SensorTemp_TPL_array.append(scale.get()*0.03+SensorTemp_TPL_array[-1])
+        SensorTemp_TPK_array.append(scale.get()*0.04+SensorTemp_TPK_array[-1])
+        SensorTemp_RT100_array.append(scale.get()*0.05+SensorTemp_RT100_array[-1])
+    if CountBtnClck % 2 == 0:
+        if SensorTemp_RTD_array[-1] <= 0:
+            SensorTemp_RTD_array.append(0)
+        else:
             SensorTemp_RTD_array.append(SensorTemp_RTD_array[-1] - random.randint(0,1))
+        if SensorTemp_Cuprum_array[-1] <= 0:
+            SensorTemp_Cuprum_array.append(0)
+        else:
             SensorTemp_Cuprum_array.append(SensorTemp_Cuprum_array[-1] - random.randint(1,3))
+        if SensorTemp_TPL_array[-1] <= 0:
+            SensorTemp_TPL_array.append(0)
+        else:
             SensorTemp_TPL_array.append(SensorTemp_TPL_array[-1] - random.randint(1,2))
+        if SensorTemp_TPK_array[-1] <= 0:
+            SensorTemp_TPK_array.append(0)
+        else:
             SensorTemp_TPK_array.append(SensorTemp_TPK_array[-1] - random.randint(1,4))
+        if SensorTemp_RT100_array[-1] <= 0:
+            SensorTemp_RT100_array.append(0)
+        else:
             SensorTemp_RT100_array.append(SensorTemp_RT100_array[-1] - random.randint(1,2))
 
-        ArrX.append(x)
-        plot.clear()
-        plot.set_xlabel('Время [cек]')
-        plot.set_ylabel('Температура [°C]')
-        plot.plot(ArrX, SensorTemp_RTD_array, 'o-c',
-                ArrX, SensorTemp_Cuprum_array, 'o--b',
-                ArrX, SensorTemp_TPL_array, 'o-.r',
-                ArrX, SensorTemp_TPK_array, 'o:g',
-                ArrX, SensorTemp_RT100_array, 'o:k')
-        canvas.draw()
-        labelRTD.config(text=str(round(SensorTemp_RTD_array[-1], 2)))
-        labelCuprum.config(text=str(round(SensorTemp_Cuprum_array[-1], 2)))
-        labelTPL.config(text=str(round(SensorTemp_TPL_array[-1], 2)))
-        labelTPK.config(text=str(round(SensorTemp_TPK_array[-1], 2)))
-        labelRT100.config(text=str(round(SensorTemp_RT100_array[-1], 2)))
-        frame.after(1000, update_plot)
-    update_plot()
 
-    # сброс графика
+    ArrX.append(x)
+    plot.clear()
+    plot.set_xlabel('Время [cек]')
+    plot.set_ylabel('Температура [°C]')
+    plot.plot(ArrX, SensorTemp_RTD_array, 'o-c',
+            ArrX, SensorTemp_Cuprum_array, 'o--b',
+            ArrX, SensorTemp_TPL_array, 'o-.r',
+            ArrX, SensorTemp_TPK_array, 'o:g',
+            ArrX, SensorTemp_RT100_array, 'o:k')
+    canvas.draw()
+    labelRTD.config(text=str(round(SensorTemp_RTD_array[-1], 2)))
+    labelCuprum.config(text=str(round(SensorTemp_Cuprum_array[-1], 2)))
+    labelTPL.config(text=str(round(SensorTemp_TPL_array[-1], 2)))
+    labelTPK.config(text=str(round(SensorTemp_TPK_array[-1], 2)))
+    labelRT100.config(text=str(round(SensorTemp_RT100_array[-1], 2)))
+    frame.after(1000, update_plot)
+    print(ArrX)
+    print(SensorTemp_RTD_array)
+    if CountBtnClck2 % 2 == 1:
+        return
+
+# сброс графика
 def reset_plot():
     global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array, start_time
     start_time = datetime.now()
@@ -130,8 +140,15 @@ def reset_plot():
 
 # Скрытие графика
 def destroy_plot():
+    global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array, start_time
+    start_time = datetime.now()
+    ArrX = [0.0]
+    SensorTemp_RTD_array = [20.0]
+    SensorTemp_Cuprum_array = [20.0]
+    SensorTemp_TPL_array = [20.0]
+    SensorTemp_TPK_array = [20.0]
+    SensorTemp_RT100_array = [20.0]
     canvas.get_tk_widget().destroy()
-
 
 # главное окно
 main = Tk()
@@ -179,8 +196,6 @@ labelCuprum.place(x=1055, y=246)
 labelTPL.place(x=1241,y=217)
 labelTPK.place(x=1305, y=323)
 labelRT100.place(x=1178, y=333)
-
-# график
 
 
 
