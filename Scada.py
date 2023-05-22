f5d92f0ad42cc17d71def2f946bc95969dcc11c1
GHDVLP <f_b2002@bk.ru>
2023-05-23 00:49:52 +0500

    Версия 1.03
    Не получилось реализовать скрытие графика без дополнительных точек при повторном открытии

--- Scada.py
+++ Scada.py
@@ -4,13 +4,24 @@ from tkinter import *
 import random
 from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from datetime import datetime, timedelta
 
+# начальные значения по умолчанию
+ArrX = [0.0]
+SensorTemp_RTD_array = [20.0]
+SensorTemp_Cuprum_array = [20.0]
+SensorTemp_TPL_array = [20.0]
+SensorTemp_TPK_array = [20.0]
+SensorTemp_RT100_array = [20.0]
+
+start_time = datetime.now()
 CountBtnClck = 1
 CountBtnClck1 = 1
 CountBtnClck2 = 1
 CountBtnClck3 = 1
+
 # функция съема значения с ползунка
 def scaleget(newVal):
     currentValue['text'] = newVal+'%'
@@ -39,10 +50,10 @@ def BtnChangeState(text):
         CountBtnClck2 += 1
         if CountBtnClck2 % 2 == 0:
             btn['bg'] = 'green'
-            create_chart_widget()
+            create_plot()
         else:
             btn['bg'] = 'grey'
-            destroy_chart_widget()
+            destroy_plot()
     if text == '4':
         global CountBtnClck3
         CountBtnClck3 += 1
@@ -51,103 +62,74 @@ def BtnChangeState(text):
         else:
             RedrawBtn['bg'] = 'grey'
 
-# создание графика
-def create_chart_widget():
-    global fig, ax, canvas
-    global x, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array, CountBtnClck3
-    ArrX = [0] # время на графике
-    SensorTemp_RTD_array = [20] # массив значений температуры RTD
-    SensorTemp_Cuprum_array = [20] # массив значений температуры Cuprum
-    SensorTemp_TPL_array = [20] # массив значений температуры TPL
-    SensorTemp_TPK_array = [20] # массив значений температуры TPK
-    SensorTemp_RT100_array = [20] # массив значений температуры RT100
-    start_time = datetime.now()
-    fig = plt.Figure(figsize=(5, 4), dpi=100)
-    ax = fig.add_subplot(111)
+# обновление графика
+def create_plot():
+    global fig, plot, canvas
+    fig = Figure(figsize=(7,4), dpi=100)
+    plot = fig.add_subplot(1, 1, 1)
     canvas = FigureCanvasTkAgg(fig, master=frame)
-    canvas.get_tk_widget().place(x=500, y=100)
-    # Обновление лейблов
-    labelRTD = Label(frame, text="", width=11, height=1, bg='grey80')
-    labelCuprum = Label(frame, text="", width=15, height=1, bg='grey80')
-    labelTPL = Label(frame, text="", width=13, height=1, bg='grey80')
-    labelTPK = Label(frame, text="", width=13, height=1, bg='grey80')
-    labelRT100 = Label(frame, text="", width=15, height=1, bg='grey80')
-
-    labelRTD.place(x=1063, y=304)
-    labelCuprum.place(x=1055, y=246)
-    labelTPL.place(x=1241, y=217)
-    labelTPK.place(x=1305, y=323)
-    labelRT100.place(x=1178, y=333)
-    # обновление графика
-    def update_chart():
-        # значения температуры текущие
+    canvas.get_tk_widget().place(x=150, y=150)
+    def update_plot():
+        global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array
         elapsed_time = datetime.now() - start_time
         x = elapsed_time.total_seconds()
         if CountBtnClck % 2 == 1:
-            global SensorTemp_Cuprum, SensorTemp_RT100, SensorTemp_RTD, SensorTemp_TPK, SensorTemp_TPL
-            SensorTemp_RTD = scale.get()*0.01+SensorTemp_RTD_array[-1]
-            SensorTemp_Cuprum = scale.get()*0.02+SensorTemp_Cuprum_array[-1]
-            SensorTemp_TPL = scale.get()*0.03+SensorTemp_TPL_array[-1]
-            SensorTemp_TPK = scale.get()*0.04+SensorTemp_TPK_array[-1]
-            SensorTemp_RT100 = scale.get()*0.05+SensorTemp_RT100_array[-1]
-            
-            SensorTemp_RTD_array.append(SensorTemp_RTD)
-            SensorTemp_Cuprum_array.append(SensorTemp_Cuprum)
-            SensorTemp_TPL_array.append(SensorTemp_TPL)
-            SensorTemp_TPK_array.append(SensorTemp_TPK)
-            SensorTemp_RT100_array.append(SensorTemp_RT100)
+            SensorTemp_RTD_array.append(scale.get()*0.01+SensorTemp_RTD_array[-1])
+            SensorTemp_Cuprum_array.append(scale.get()*0.02+SensorTemp_Cuprum_array[-1])
+            SensorTemp_TPL_array.append(scale.get()*0.03+SensorTemp_TPL_array[-1])
+            SensorTemp_TPK_array.append(scale.get()*0.04+SensorTemp_TPK_array[-1])
+            SensorTemp_RT100_array.append(scale.get()*0.05+SensorTemp_RT100_array[-1])
         if CountBtnClck % 2 == 0:
-            SensorTemp_RTD -= random.randint(0,1)
-            if SensorTemp_RTD <= 0:
-                SensorTemp_RTD = 0
-            SensorTemp_Cuprum -= random.randint(1,3)
-            if SensorTemp_Cuprum <= 0:
-                SensorTemp_Cuprum = 0
-            SensorTemp_TPL -= random.randint(1,2)
-            if SensorTemp_TPL <= 0:
-                SensorTemp_TPL = 0
-            SensorTemp_TPK -= random.randint(1,4)
-            if SensorTemp_TPK <= 0:
-                SensorTemp_TPK = 0
-            SensorTemp_RT100 -= random.randint(1,2)
-            if SensorTemp_RT100 <= 0:
-                SensorTemp_RT100 = 0
+            if SensorTemp_RTD_array[-1] <= 0:
+                SensorTemp_RTD_array[-1] = 0
+            if SensorTemp_Cuprum_array[-1]  <= 0:
+                SensorTemp_Cuprum_array[-1]  = 0
+            if SensorTemp_TPL_array[-1] <= 0:
+                SensorTemp_TPL_array[-1] = 0
+            if SensorTemp_TPK_array[-1] <= 0:
+                SensorTemp_TPK_array[-1] = 0
+            if SensorTemp_RT100_array[-1] <= 0:
+                SensorTemp_RT100_array[-1] = 0
 
-            SensorTemp_RTD_array.append(SensorTemp_RTD)
-            SensorTemp_Cuprum_array.append(SensorTemp_Cuprum)
-            SensorTemp_TPL_array.append(SensorTemp_TPL)
-            SensorTemp_TPK_array.append(SensorTemp_TPK)
-            SensorTemp_RT100_array.append(SensorTemp_RT100)
+            SensorTemp_RTD_array.append(SensorTemp_RTD_array[-1] - random.randint(0,1))
+            SensorTemp_Cuprum_array.append(SensorTemp_Cuprum_array[-1] - random.randint(1,3))
+            SensorTemp_TPL_array.append(SensorTemp_TPL_array[-1] - random.randint(1,2))
+            SensorTemp_TPK_array.append(SensorTemp_TPK_array[-1] - random.randint(1,4))
+            SensorTemp_RT100_array.append(SensorTemp_RT100_array[-1] - random.randint(1,2))
 
         ArrX.append(x)
-        ax.clear()
-        ax.set_xlabel('Время [cек]')
-        ax.set_ylabel('Температура [°C]')
-        ax.plot(ArrX, SensorTemp_RTD_array, 'o-c',
+        plot.clear()
+        plot.set_xlabel('Время [cек]')
+        plot.set_ylabel('Температура [°C]')
+        plot.plot(ArrX, SensorTemp_RTD_array, 'o-c',
                 ArrX, SensorTemp_Cuprum_array, 'o--b',
                 ArrX, SensorTemp_TPL_array, 'o-.r',
                 ArrX, SensorTemp_TPK_array, 'o:g',
                 ArrX, SensorTemp_RT100_array, 'o:k')
-        canvas.draw_idle()
+        canvas.draw()
         labelRTD.config(text=str(round(SensorTemp_RTD_array[-1], 2)))
         labelCuprum.config(text=str(round(SensorTemp_Cuprum_array[-1], 2)))
         labelTPL.config(text=str(round(SensorTemp_TPL_array[-1], 2)))
         labelTPK.config(text=str(round(SensorTemp_TPK_array[-1], 2)))
         labelRT100.config(text=str(round(SensorTemp_RT100_array[-1], 2)))
-        frame.after(1000, update_chart)
-    update_chart()
-    if CountBtnClck3 % 2 == 0:
-        return
+        frame.after(1000, update_plot)
+    update_plot()
+
+    # сброс графика
+def reset_plot():
+    global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array, start_time
+    start_time = datetime.now()
+    ArrX = [0.0]
+    SensorTemp_RTD_array = [20.0]
+    SensorTemp_Cuprum_array = [20.0]
+    SensorTemp_TPL_array = [20.0]
+    SensorTemp_TPK_array = [20.0]
+    SensorTemp_RT100_array = [20.0]
+    plot.clear()
+    canvas.draw()
 
 # Скрытие графика
-def destroy_chart_widget():
-    global ArrX, SensorTemp_RTD_array, SensorTemp_Cuprum_array, SensorTemp_TPL_array, SensorTemp_TPK_array, SensorTemp_RT100_array
-    ArrX = [0]
-    SensorTemp_RTD_array = [20]
-    SensorTemp_Cuprum_array = [20]
-    SensorTemp_TPL_array = [20]
-    SensorTemp_TPK_array = [20]
-    SensorTemp_RT100_array = [20]
+def destroy_plot():
     canvas.get_tk_widget().destroy()
 
 
@@ -170,11 +152,11 @@ frame.pack()
 AutoRegulation = Button(frame, text='Авт. вентилирование', bg = 'grey', width=21, height=2, state = None, command=lambda: BtnChangeState('1'))
 ManualRegulation = Button(frame, text='Вкл. вентилирование', bg = 'grey', width=21, height=2, state = None, command=lambda: BtnChangeState('2'))
 btn = Button(frame, text='Запуск/стоп', bg = 'grey', width=21, height=2, state = None, command=lambda: BtnChangeState('3'))
-RedrawBtn = Button(frame, text='Сбросить значения', bg = 'grey', width=21, height=2, command=lambda: BtnChangeState('4'))
+RedrawBtn = Button(frame, text='Сбросить значения', bg = 'grey', width=21, height=2, command=reset_plot)
 AutoRegulation.place(x=1011, y=619)
 ManualRegulation.place(x=1010, y=662)
 btn.place(x=50, y=50)
-RedrawBtn.place(x = 711, y = 513)
+RedrawBtn.place(x = 150, y = 575)
 
 # описание ползунка
 scale_tittle = Label(frame, text = 'Нагрев %', width=10, height=1, bg = 'grey80')
@@ -197,4 +179,9 @@ labelCuprum.place(x=1055, y=246)
 labelTPL.place(x=1241,y=217)
 labelTPK.place(x=1305, y=323)
 labelRT100.place(x=1178, y=333)
+
+# график
+
+
+
 main.mainloop()
